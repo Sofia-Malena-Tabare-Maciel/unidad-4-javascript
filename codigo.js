a210115d71fa94f8bacbe3d3ca3556045bd2c5be
SmalenaTm <nemrod.nomad@gmail.com>
2023-07-08 12:05:16 -0300

    .

---
+++ codigo.js
@@ -0,0 +1,12 @@
+var pie = 1000
+var bicicleta = 10000
+var colectivo = 30000
+var auto = 100000
+var avión = 100000
+var resultado = pie + bicicleta + colectivo + auto + avión
+console.log("Yendo a pie, en bicicleta, en colectivo, en auto y en avión hice aproximadamente en kilometros " + resultado) 
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+console.log(10>0); 
